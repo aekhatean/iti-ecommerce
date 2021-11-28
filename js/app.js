6a51c2b9436faed8d9cb682331b5123df2cfa8b1
Adham Khatean <aekhatean@gmail.com>
2021-11-28 15:58:58 +0200

    Enhance cart.html initial UI

--- js/app.js
+++ js/app.js
@@ -42,12 +42,14 @@ function addToCart(purchaseButton) {
 }
 
 const visibleCartProducts = document.getElementById("cart-products-section");
+
+// Show cart products on cart page load
 function showCart() {
   let currentCart = JSON.parse(localStorage.getItem("userCart"));
   for (purchase in currentCart) {
     const productIndex = parseInt(currentCart[purchase]);
     // console.log(productIndex);
-    fetch("https://fakestoreapi.com/products")
+    fetch("../api/products.json")
       .then((res) => res.json())
       .then((data) => {
         let product = data[productIndex];
@@ -55,18 +57,18 @@ function showCart() {
         //Very bad performance, preferably store products data in full instead
         const item = document.createElement("div");
         item.innerHTML = `
-        <div class ="cart-product-card grid-item" key="${product.id}">
-        <img class="products-img" src="${product.image}"/>
-          <p class="products-title fs-300">${
-            product.title.length > 40
-              ? product.title.substr(0, 40) + ".. "
-              : product.title
-          }</p>
-          <div class="products-price-rating flex">
-              <p class="products-price fs-400"><b>${product.price}</b>$</p>
-              <p class="products-rating flex fs-400">${
-                product.rating.rate
-              }<img src="../assets/Filled_star.png" /></p>`;
+        <div class ="cart-product-card grid" key="${product.id}">
+        <img class="cart-product-img" src="${product.image}"/>
+        <b class="cart-product-title fs-300">${
+          product.title.length > 40
+            ? product.title.substr(0, 40) + ".. "
+            : product.title
+        }</b>
+        <p class="cart-product-price fs-400">Price: <b>${product.price}</b>$</p>
+        <p class="cart-product-count flex fs-400">count : 1</p>
+        <p class="cart-product-total">Total: <b>${product.price}</b>$</p>
+        </div>
+              <hr />`;
         if (location.href.indexOf("cart.html") > -1)
           visibleCartProducts.appendChild(item);
       });
