f3094efe3ed93b2c4b22ab8ec25e2a34a39573b5
Adham Khatean <aekhatean@gmail.com>
2021-11-28 02:22:31 +0200

    Create cart page initial layout and logic

--- js/app.js
+++ js/app.js
@@ -29,3 +29,46 @@ fetch("../api/products.json")
         productsGrid.appendChild(item);
     })
   );
+
+// cart page
+const cartProducts = Array();
+function addToCart(purchaseButton) {
+  const productId = purchaseButton.parentNode.attributes.key.value;
+  cartProducts.push(productId);
+  if (cartProducts.length > 0) {
+    localStorage.setItem("userCart", JSON.stringify(cartProducts));
+  }
+  console.log(cartProducts);
+}
+
+const visibleCartProducts = document.getElementById("cart-products-section");
+function showCart() {
+  let currentCart = JSON.parse(localStorage.getItem("userCart"));
+  for (purchase in currentCart) {
+    const productIndex = parseInt(currentCart[purchase]);
+    // console.log(productIndex);
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((data) => {
+        let product = data[productIndex];
+        console.log(product);
+        //Very bad performance, preferably store products data in full instead
+        const item = document.createElement("div");
+        item.innerHTML = `
+        <div class ="cart-product-card grid-item" key="${product.id}">
+        <img class="products-img" src="${product.image}"/>
+          <p class="products-title fs-300">${
+            product.title.length > 40
+              ? product.title.substr(0, 40) + ".. "
+              : product.title
+          }</p>
+          <div class="products-price-rating flex">
+              <p class="products-price fs-400"><b>${product.price}</b>$</p>
+              <p class="products-rating flex fs-400">${
+                product.rating.rate
+              }<img src="../assets/Filled_star.png" /></p>`;
+        if (location.href.indexOf("cart.html") > -1)
+          visibleCartProducts.appendChild(item);
+      });
+  }
+}
