31442a9ca7a325698d4aa83b20874a140a20fa81
ahmed mahmoud <52626564+ahmedsaleh1998@users.noreply.github.com>
2021-11-30 17:37:38 +0200

    Make user validation
    1- check if find user in users
    2- make sign in validation
    3- make logout
    4-make current user

--- js/checkout.js
+++ js/checkout.js
@@ -1,13 +1,18 @@
 function fillinformation() {
-    var user = localStorage.getItem('allUsers');
-    var user2 = JSON.parse(user);
+    if (localStorage.getItem('currentUser')) {
+        var user = localStorage.getItem('currentUser');
+        var user2 = JSON.parse(user);
 
-    document.getElementById("F_name").value = user2[0].F_name;
-    document.getElementById("L_name").value = user2[0].L_name;
-    document.getElementById("Phone").value = user2[0].Phone;
-    document.getElementById("City").value = user2[0].City;
-    document.getElementById("Street").value = user2[0].Street;
-    document.getElementById("Email").value = user2[0].Email;
+        document.getElementById("F_name").value = user2[0].F_name;
+        document.getElementById("L_name").value = user2[0].L_name;
+        document.getElementById("Phone").value = user2[0].Phone;
+        document.getElementById("City").value = user2[0].City;
+        document.getElementById("Street").value = user2[0].Street;
+        document.getElementById("Email").value = user2[0].Email;
+    } else {
+        alert("you should signin")
+    }
+
 }
 
 fillinformation();
@@ -15,14 +20,20 @@ fillinformation();
 
 
 
-function submmition() {
+function submmition(event) {
 
-    var x = [{ firstname: document.getElementById("F_name").value, lastname: document.getElementById("L_name").value, Phone: document.getElementById("Phone").value, City: document.getElementById("City").value, Street: document.getElementById("Street").value, Email: document.getElementById("Email").value }]
+    if (localStorage.getItem('currentUser')) {
+        var x = [{ firstname: document.getElementById("F_name").value, lastname: document.getElementById("L_name").value, Phone: document.getElementById("Phone").value, City: document.getElementById("City").value, Street: document.getElementById("Street").value, Email: document.getElementById("Email").value }]
 
-    try {
-        localStorage.setItem("orders", JSON.stringify(x))
-    } catch (error) {
-        alert(error)
+        try {
+            localStorage.setItem("orders", JSON.stringify(x))
+        } catch (error) {
+            alert(error)
+        }
+    } else {
+        event.preventDefault(); {
+            alert("you should signin")
+        }
     }
 
 
--- js/nav.js
+++ js/nav.js
@@ -1,32 +1,35 @@
 //navbar handling
 
 function showNav() {
-  document.getElementById("nav-links").style.display = "flex";
+    document.getElementById("nav-links").style.display = "flex";
 }
+
 function hideNav() {
-  setTimeout(hide, 3000);
+    setTimeout(hide, 3000);
 }
 
 function hide() {
-  document.getElementsByClassName("hide")[0].style.visibilty = "visible";
-  document.getElementById("nav-links").style.display = "none";
+    document.getElementsByClassName("hide")[0].style.visibilty = "visible";
+    document.getElementById("nav-links").style.display = "none";
 }
 
 //username
 
 const nav_username = document.getElementById("authed-username");
-function displayUser() {
-  var user = localStorage.getItem("user");
 
-  if (!user) {
-    nav_username.innerHTML = "Please Sign in!";
-  } else if (user) {
-    let logout = document.createElement("span");
-    logout.innerHTML = "Logout";
-    logout.addEventListener("click", () => {
-      nav_username.innerHTML = "Please Sign in!";
-    });
-    nav_username.innerHTML = user.split(",")[0];
-    nav_username.appendChild(logout);
-  }
+function displayUser() {
+    if (localStorage.getItem("currentUser")) {
+        var user = JSON.parse(localStorage.getItem("currentUser"));
+        let logout = document.createElement("span");
+        logout.innerHTML = "  Logout";
+        logout.addEventListener("click", () => {
+
+            localStorage.removeItem("currentUser")
+        });
+        nav_username.innerHTML = user[0].Username + "    ";
+        nav_username.appendChild(logout);
+    } else {
+        nav_username.innerHTML = "Please Sign in!";
+    }
 }
+displayUser();
